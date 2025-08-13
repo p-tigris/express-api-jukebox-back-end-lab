77a07ffcbf57d47fca04ae61736062a27860edda
smdhar <shoumik_dhar@yahoo.com>
2025-08-13 16:28:47 -0400

    Lab setup complete

---
+++ controllers/tracks.js
@@ -0,0 +1,6 @@
+const Track = require('../models/track.js');
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const methodOverride = require('method-override');
+
+const trackController = require('./controllers/tracks.js');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors);
+app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/tracks', trackController);
+
+app.listen(3000, () => {
+    console.log('The express app is ready!');
+});
